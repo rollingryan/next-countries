47e4dc522d9572f2cacefa34440acff0ac56b81d
Ryan Donald <ryan@rollingcreative.co.za>
2022-06-04 09:00:41 +0100

    Add border conditional

--- src/components/BorderCountries/index.tsx
+++ src/components/BorderCountries/index.tsx
@@ -4,10 +4,14 @@ interface Props {
   borders: string[];
 }
 
-const BorderCountries: React.FC<Props> = ({ borders }) => (
-  <Typography>{`Borders: ${borders.map(
-    (border: string) => ` ${border}`
-  )}`}</Typography>
-);
+const BorderCountries: React.FC<Props> = ({ borders }) => {
+  return (
+    (borders.length > 0 && (
+      <Typography>{`Borders: ${borders.map(
+        (border: string) => ` ${border}`
+      )}`}</Typography>
+    )) || <Typography>This country is an island &#127965;&#65039;</Typography>
+  );
+};
 
 export default BorderCountries;
