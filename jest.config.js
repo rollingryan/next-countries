b4504017977ac596799beca33c113083949c6798
Ryan Donald <ryan@rollingcreative.co.za>
2022-06-05 14:52:25 +0100

    Setup jsdom

--- jest.config.js
+++ jest.config.js
@@ -25,6 +25,7 @@ module.exports = {
   ],
 
   testRegex: "((\\.|/)(test))\\.j?t?sx?$",
+  testEnvironment: "jsdom",
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
